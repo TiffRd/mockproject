5c813abf674c8910476376c73f93089af1158052
Tiff <156123940+TiffRd@users.noreply.github.com>
2024-01-18 11:18:30 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-
+let stick = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . b b b b b b . . . . . . 
+    . . . . b b b b b b b . . . . . 
+    . . . . b b b b b b b . . . . . 
+    . . . . . b b b b b b . . . . . 
+    . . . . . b b b b b b . . . . . 
+    . . . . . . b b b . . . . . . . 
+    . . . . . . . b . . . . . . . . 
+    . . . b b b . b . . . . . . . . 
+    . . . . . b b b b b b b b b . . 
+    . . . . . . . b . . . . . . . . 
+    . . . . . . b b . . . . . . . . 
+    . . . . . b b b b . . . . . . . 
+    . . . . b b . . b b . . . . . . 
+    . . . . b . . . . b . . . . . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(stick)
+controller.moveSprite(stick)
+stick.setStayInScreen(true)
+tiles.setCurrentTilemap(tilemap`level1`)
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000010101010101010101010101010101010202020202020202020202020202020203030303030303030303030303030303030303030403030303030303030303030303030304030303040303030303030303030303040303030403030303030303030303030403030304030303030303030303030304030303040303030303030303030303040303030403030303030303030303030403030303030303030303030303030304030303040303030303030303030303030303030403030303030303020202020202020204020202020202020101010101010101010101010101010100000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.floorLightMoss], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
