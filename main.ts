4f134ac635bbd4462787381d8010d4b7e1a7a971
Tiff <156123940+TiffRd@users.noreply.github.com>
2024-01-18 12:02:05 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -21,23 +21,23 @@ controller.moveSprite(stick)
 stick.setStayInScreen(true)
 tiles.setCurrentTilemap(tilemap`level1`)
 let twif = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . 3 3 3 3 . . . . . . . . 
-    . . . 3 3 3 3 3 3 3 . . . . . . 
-    . . . 3 3 3 3 3 3 3 3 3 . . . . 
-    . . . 3 3 3 3 3 3 3 3 3 3 . . . 
-    . . . 3 3 3 3 3 3 3 3 3 3 3 . . 
-    . . 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
-    . . 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    . . . 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    . . . . 3 3 3 3 3 3 3 3 3 3 3 3 
-    . . . . . 3 3 3 3 3 3 3 3 3 3 3 
-    . . . . . . 3 3 3 3 3 3 3 3 3 3 
-    . . . . . . . . 3 3 3 3 3 3 3 . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . 3 3 3 3 3 . . . . . . . .
+    . f f f f 3 3 3 3 3 3 . . . . .
+    f f f f f f 3 3 f f 3 3 . . . .
+    f f f f 1 f 3 f f f f 3 3 . . .
+    f 1 f f 1 f f 1 1 1 1 f 3 3 . .
+    f 1 1 1 1 f f 1 1 1 1 1 f 3 . .
+    f f 1 1 1 f 1 1 1 1 1 1 f 3 3 .
+    . f f f f f f 1 1 1 f f f 3 3 3
+    . . 3 3 3 3 f 1 1 f f f f 3 3 3
+    . . 3 3 1 3 f f 1 f f f 3 3 3 3
+    . . 3 3 3 3 3 f f f f f 3 3 3 3
+    . . . 3 3 3 3 3 3 3 3 3 3 3 3 3
+    . . . . . 3 3 3 3 3 3 3 3 3 3 .
+    . . . . . . . . . . . . . . . .
+`, SpriteKind.Player)
 controller.player2.moveSprite(twif)
 scene.cameraFollowSprite(twif)
 controller.moveSprite(twif)
