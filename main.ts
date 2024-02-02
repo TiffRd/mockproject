590be31d3d8d64d50087be1d5c0b1ce757cd43c2
Tiff <156123940+TiffRd@users.noreply.github.com>
2024-02-01 23:36:57 -0600

    game complete last touches added, got rid of miku:(

--- main.ts
+++ main.ts
@@ -15,20 +15,16 @@ function show_character_picker () {
     showCharacterScreen = true
     Huge_stick = sprites.create(assets.image`huge stick`, SpriteKind.SelectableCharacter)
     Huge_stick.z = 101
-    Huge_stick.setPosition(32, 67)
+    Huge_stick.setPosition(45, 67)
     sprites.setDataSprite(Huge_stick, "player", stick)
     textSprite = textsprite.create("Choose Your Character", 11, 15)
     textSprite.z = 101
     textSprite.setPosition(80, 29)
     Big_twif = sprites.create(assets.image`BIG TWIF`, SpriteKind.SelectableCharacter)
     Big_twif.z = 101
-    Big_twif.setPosition(73, 67)
+    Big_twif.setPosition(90, 67)
     sprites.setDataSprite(Big_twif, "player", twif)
-    Big_miku = sprites.create(assets.image`miku0`, SpriteKind.SelectableCharacter)
-    Big_miku.z = 101
-    Big_miku.setPosition(119, 67)
-    sprites.setDataSprite(Big_miku, "player", miku)
-    ourCharacters = [Huge_stick, Big_twif, Big_miku]
+    ourCharacters = [Huge_stick, Big_twif]
     current_character_index = 1
     character_selector_box = sprites.create(img`
         cccccccccccccccccccccccccccccccccccc
@@ -70,21 +66,21 @@ function show_character_picker () {
         `, SpriteKind.Backround)
     character_selector_box.z = 101
 }
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthEast1, function (sprite, location) {
+    controller.moveSprite(sprite, 100, 100)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    controller.moveSprite(sprite, 30, 30)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     tiles.setCurrentTilemap(tilemap`level1`)
     if (showCharacterScreen) {
         if (current_character_index == 0) {
             thePlayer = stick
             sprites.destroy(twif)
-            sprites.destroy(miku)
-        } else if (current_character_index == 0) {
-            thePlayer = miku
-            sprites.destroy(stick)
-            sprites.destroy(twif)
         } else {
             thePlayer = twif
             sprites.destroy(stick)
-            sprites.destroy(miku)
         }
         tiles.placeOnRandomTile(thePlayer, sprites.dungeon.darkGroundNorthEast1)
         scene.cameraFollowSprite(thePlayer)
@@ -101,46 +97,35 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         showCharacterScreen = false
     }
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
-    controller.moveSprite(sprite, 30, 30)
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile7`, function (sprite, location) {
+    game.gameOver(true)
 })
-controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+    tiles.setCurrentTilemap(tilemap`maybe final`)
+    info.setLife(5)
+    tiles.placeOnTile(sprite, tiles.getTileLocation(14, 1))
+    sprite.setVelocity(100, 100)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     if (showCharacterScreen) {
         current_character_index = (current_character_index + 1) % ourCharacters.length % ourCharacters.length
     }
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile7`, function (sprite, location) {
-    game.gameOver(true)
-})
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
-    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
-    tiles.setCurrentTilemap(tilemap`final level`)
-    tiles.placeOnTile(sprite, tiles.getTileLocation(14, 1))
-})
-spriteutils.createRenderable(100, function (screen2) {
-    if (showCharacterScreen) {
-        screen2.fill(11)
-        currently_selected_character = ourCharacters[current_character_index]
-        character_selector_box.setPosition(currently_selected_character.x, currently_selected_character.y)
+function Enemy_respawn () {
+    for (let index = 0; index < 1; index++) {
+        enemy_sprite = sprites.create(Enemy_images._pickRandom(), SpriteKind.Enemy)
+        tiles.placeOnTile(enemy_sprite, Spawn_location.removeAt(randint(0, Spawn_location.length - 0)))
+        enemy_sprite.setBounceOnWall(true)
+        enemy_sprite.setVelocity(randint(-50, 50), randint(-50, 50))
     }
-})
-scene.onOverlapTile(SpriteKind.Player, assets.tile`button not`, function (sprite, location) {
-    tiles.placeOnTile(sprite, tiles.getTileLocation(16, 28))
-})
-scene.onOverlapTile(SpriteKind.Player, assets.tile`pink button`, function (sprite, location) {
-    tiles.placeOnTile(sprite, tiles.getTileLocation(23, 21))
+}
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthWest1, function (sprite, location) {
+    controller.moveSprite(sprite, 100, 100)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
     tiles.setTileAt(tiles.getTileLocation(15, 6), assets.tile`myTile2`)
     tiles.setTileAt(tiles.getTileLocation(10, 13), assets.tile`myTile3`)
-    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
-})
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile8`, function (sprite, location) {
-    tiles.setTileAt(tiles.getTileLocation(15, 24), assets.tile`myTile7`)
-    tiles.setTileAt(tiles.getTileLocation(15, 29), assets.tile`myTile9`)
     music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -148,18 +133,29 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         current_character_index = (current_character_index + 1) % ourCharacters.length
     }
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`orange button`, function (sprite, location) {
-    tiles.placeOnTile(sprite, tiles.getTileLocation(15, 19))
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
+    tiles.placeOnRandomTile(sprite, sprites.dungeon.darkGroundSouthWest0)
+    info.changeLifeBy(-1)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorClosedSouth, function (sprite, location) {
+    game.gameOver(false)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile8`, function (sprite, location) {
+    tiles.setTileAt(tiles.getTileLocation(1, 21), assets.tile`myTile7`)
+    tiles.setTileAt(tiles.getTileLocation(14, 28), assets.tile`myTile9`)
+    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level6`)
     tiles.placeOnTile(sprite, tiles.getTileLocation(14, 1))
-    info.setLife(3)
+    info.setLife(5)
     Spawn_location = tiles.getTilesByType(sprites.dungeon.darkGroundSouthWest0)
     Enemy_images = [assets.image`Twif`, assets.image`Stick`, assets.image`myImage1`]
-    for (let index = 0; index < 15; index++) {
+    for (let index = 0; index < 5; index++) {
         enemy_sprite = sprites.create(Enemy_images._pickRandom(), SpriteKind.Enemy)
         tiles.placeOnTile(enemy_sprite, Spawn_location.removeAt(randint(0, Spawn_location.length - 0)))
+        enemy_sprite.setBounceOnWall(true)
+        enemy_sprite.setVelocity(randint(-50, 50), randint(-50, 50))
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
@@ -170,48 +166,40 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, l
 info.onLifeZero(function () {
     game.gameOver(false)
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundSouthEast1, function (sprite, location) {
+    controller.moveSprite(sprite, 100, 100)
+})
+spriteutils.createRenderable(100, function (screen2) {
+    if (showCharacterScreen) {
+        screen2.fill(11)
+        currently_selected_character = ourCharacters[current_character_index]
+        character_selector_box.setPosition(currently_selected_character.x, currently_selected_character.y)
+    }
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundSouthWest0, function (sprite, location) {
     controller.moveSprite(sprite, 100, 100)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
+    sprites.destroy(otherSprite)
+    Enemy_respawn()
 })
-let enemy_sprite: Sprite = null
-let Enemy_images: Image[] = []
-let Spawn_location: tiles.Location[] = []
 let currently_selected_character: Sprite = null
+let Spawn_location: tiles.Location[] = []
+let Enemy_images: Image[] = []
+let enemy_sprite: Sprite = null
 let thePlayer: Sprite = null
 let character_selector_box: Sprite = null
 let current_character_index = 0
 let ourCharacters: Sprite[] = []
-let Big_miku: Sprite = null
 let Big_twif: Sprite = null
 let textSprite: TextSprite = null
 let Huge_stick: Sprite = null
 let showCharacterScreen = false
-let miku: Sprite = null
 let twif: Sprite = null
 let stick: Sprite = null
 scene.setBackgroundImage(assets.image`DUNGEON MAZE`)
 pauseUntil(() => controller.A.isPressed())
 stick = sprites.create(assets.image`myImage`, SpriteKind.Player)
 twif = sprites.create(assets.image`myImage0`, SpriteKind.Player)
-miku = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . 9 . . . . . . 9 . . . . 
-    . . . 9 9 9 . 9 . . 9 9 9 . . . 
-    . . 9 9 9 9 9 9 9 9 9 . 9 9 . . 
-    . . 9 9 9 9 9 9 9 9 9 9 . 9 . . 
-    . 9 9 . 9 9 d 9 9 9 9 9 . 9 9 . 
-    . 9 9 . 9 9 6 d d 6 9 9 . 9 9 . 
-    . 9 9 9 9 d 6 d d 6 d 9 9 9 9 . 
-    . 9 9 9 . 3 d d d d 3 9 9 9 9 . 
-    . 9 9 9 . . 1 d d 1 . . . 9 . . 
-    . 9 9 . . d 1 1 6 1 d . 9 9 . . 
-    . 9 9 . f d f 1 6 f d f 9 9 . . 
-    . . 9 9 . f f f f f f 9 9 . . . 
-    . . . 9 9 9 f f f f 9 9 . . . . 
-    . . . . . . f . . f . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
 show_character_picker()
